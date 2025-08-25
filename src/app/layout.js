1b36156545e2fe2efeee3d692ffdc1f9ea322124
Aayush Neupane <aayushneupane55@gmail.com>
2025-08-25 13:14:54 -0400

    Done

--- src/app/layout.js
+++ src/app/layout.js
@@ -48,10 +48,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="font-sans">
-        <link
-          href="https://fonts.googleapis.com/css2?family=Andada+Pro:ital,wght@0,400..840;1,400..840&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
         <Navbar />
         <ContentWrapper>{children}</ContentWrapper>
       </body>
